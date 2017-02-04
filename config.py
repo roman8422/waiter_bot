642a1e79aab33287b2ba76628be1b69f43050081
Roman Vrublevskiy <rvrublevskiy@tango.me>
2017-02-04 12:57:08 +0000

    Disable BOT_EXTRA_PLUGIN_DIR

--- config.py
+++ config.py
@@ -48,5 +48,5 @@ CHATROOM_PRESENCE = ('SPb lunch',
 )
 CHATROOM_FN = 'LunchBot'
 
-BOT_EXTRA_PLUGIN_DIR = '/local/errbot-plugins/'
+# BOT_EXTRA_PLUGIN_DIR = '/local/errbot-plugins/'
 BOT_ALT_PREFIXES = ('Err', '@LunchBot',)

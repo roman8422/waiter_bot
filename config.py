94effc895d440cd6c74f39052d8f81ac53daad8c
Roman Vrublevskiy <rvrublevskiy@tango.me>
2017-02-04 12:24:34 +0000

    Move credentials to separate file

--- config.py
+++ config.py
@@ -1,4 +1,27 @@
 import logging
+from pathlib import Path
+
+from prod_creds import BOT_IDENTITY
+
+if Path('dev_creds.py').is_file():
+    from dev_creds import BOT_IDENTITY
+
+## BOT_IDENTITY should be imported from separate file that's not checked out to git
+## Example:
+#BOT_IDENTITY = {
+#    ## HipChat mode (Comment the above if using this mode)
+#    'username' : '1_2@chat.btf.hipchat.com',
+#    'password' : '123qweASD',
+#    ## Group admins can create/view tokens on the settings page after logging
+#    ## in on HipChat's website
+#    'token'    : 'Gu9OWkcnJMufrAwg3sHy8Ali87icGuJhFI6auUeO',
+#    ## If you're using HipChat server (self-hosted HipChat) then you should set
+#    ## the endpoint below. If you don't use HipChat server but use the hosted version
+#    ## of HipChat then you may leave this commented out.
+#    'endpoint' : 'https://hipchat.test.intra',
+#    'verify': False,
+#}
+
 
 # This is a minimal configuration to get you started with the Text mode.
 # If you want to connect Errbot to chat services, checkout
@@ -18,19 +41,7 @@ BOT_ADMINS = (
         '@Roman',
 )  # !! Don't leave that to "CHANGE ME" if you connect your errbot to a chat system !!
 
-BOT_IDENTITY = {
-    ## HipChat mode (Comment the above if using this mode)
-    'username' : '1_2@chat.btf.hipchat.com',
-    'password' : '123qweASD',
-    ## Group admins can create/view tokens on the settings page after logging
-    ## in on HipChat's website
-    'token'    : 'Gu9OWkcnJMufrAwg3sHy8Ali87icGuJhFI6auUeO',
-    ## If you're using HipChat server (self-hosted HipChat) then you should set
-    ## the endpoint below. If you don't use HipChat server but use the hosted version
-    ## of HipChat then you may leave this commented out.
-    'endpoint' : 'https://hipchat.test.intra',
-    'verify': False,
-}
+)  # !! Don't leave that to "CHANGE ME" if you connect your errbot to a chat system !!
 
 XMPP_CA_CERT_FILE = None
 CHATROOM_PRESENCE = ('SPb lunch',
---
+++ dev_creds.py
@@ -0,0 +1,14 @@
+BOT_IDENTITY = {
+    ## HipChat mode (Comment the above if using this mode)
+    'username' : '1_2@chat.btf.hipchat.com',
+    'password' : '123qweASD',
+    ## Group admins can create/view tokens on the settings page after logging
+    ## in on HipChat's website
+    'token'    : 'Gu9OWkcnJMufrAwg3sHy8Ali87icGuJhFI6auUeO',
+    ## If you're using HipChat server (self-hosted HipChat) then you should set
+    ## the endpoint below. If you don't use HipChat server but use the hosted version
+    ## of HipChat then you may leave this commented out.
+    'endpoint' : 'https://hipchat.test.intra',
+    'verify': False,
+}
+

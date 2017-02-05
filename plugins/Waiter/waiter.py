de4056cb713d600795c72c214985da100d12db91
Roman Vrublevskiy <rvrublevskiy@tango.me>
2017-02-05 19:21:22 +0000

    Add order_add command

--- plugins/Waiter/waiter.py
+++ plugins/Waiter/waiter.py
@@ -7,3 +7,17 @@ class Waiter(BotPlugin):
     def hello(self, msg, args):
         """Say hello to the world"""
         return "Hello, waiter!"
+
+    # @botcmd(split_args_with=None)
+    @botcmd()
+    def order_add(self, msg, args):
+        """Make your order. Format: !orderadd restorant[:] 'place order text here'"""
+        _restaurant = args.split(' ')[0].strip(' :')
+        _order_content = args.replace(_restaurant, '')
+        _retern_message = ""
+        _retern_message += "/me accepted following order:\n"
+        _retern_message += "From: {}\n".format(msg.frm.nick)
+        _retern_message += "Restaurant: {}\n".format(_restaurant)
+        _retern_message += "Order content: {}\n".format(_order_content)
+
+        return _retern_message
